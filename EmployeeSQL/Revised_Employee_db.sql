740530184966b719361d84bb67b80cc644ff85e1
AbelDumecha <Abel@montes-mbp.mynetworksettings.com>
2024-05-28 16:29:17 -0400

    Revised as per the comment

--- EmployeeSQL/Revised_Employee_db.sql
+++ EmployeeSQL/Revised_Employee_db.sql
@@ -1,66 +1,66 @@
 ---- 1. Data Modeling and Engineering
 ----- Create tables of Employees, Departments, dept_managers, dept_emp, salaries, & Titles
-CREATE TABLE Employees (
-    emp_no INT   NOT NULL,
-    emp_title_id VARCHAR(255)   NOT NULL,
-    birth_date Date   NOT NULL,
-    first_name VARCHAR(255)   NOT NULL,
-    last_name VARCHAR(255)   NOT NULL,
-    sex VARCHAR(255)   NOT NULL,
-    hire_date Date   NOT NULL,
-    CONSTRAINT pk_Employees PRIMARY KEY (
-        emp_no
+CREATE TABLE "employees" (
+    "emp_no" INT   NOT NULL,
+    "emp_title_id" VARCHAR(6)   NOT NULL,
+    "birth_date" Date   NOT NULL,
+    "first_name" VARCHAR(255)   NOT NULL,
+    "last_name" VARCHAR(255)   NOT NULL,
+    "sex" VARCHAR(4)   NOT NULL,
+    "hire_date" Date   NOT NULL,
+    CONSTRAINT "pk_employees" PRIMARY KEY (
+        "emp_no"
      )
 );
 
-CREATE TABLE Departments (
-    dept_no VARCHAR(255)   NOT NULL,
-    dept_name VARCHAR(255)   NOT NULL,
-    CONSTRAINT pk_Departments PRIMARY KEY (
-        dept_no
+CREATE TABLE "departments" (
+    "dept_no" VARCHAR(8)   NOT NULL,
+    "dept_name" VARCHAR(255)   NOT NULL,
+    CONSTRAINT "pk_departments" PRIMARY KEY (
+        "dept_no"
      )
 );
 
-CREATE TABLE Dept_managers (
-    dept_no VARCHAR(255)   NOT NULL,
-    emp_no INT   NOT NULL
+CREATE TABLE "dept_employees" (
+    "emp_no" INT   NOT NULL,
+    "dept_no" VARCHAR(8)   NOT NULL
 );
 
-CREATE TABLE Dept_employees (
-    emp_no INT   NOT NULL,
-    dept_no VARCHAR(255)   NOT NULL
+CREATE TABLE "dept_managers" (
+    "dept_no" VARCHAR(8)   NOT NULL,
+    "emp_no" INT   NOT NULL
 );
 
-CREATE TABLE Salaries (
-    emp_no INT   NOT NULL,
-    salary INT   NOT NULL
+CREATE TABLE "salaries" (
+    "emp_no" INT   NOT NULL,
+    "salary" INT   NOT NULL
 );
 
-CREATE TABLE Titles (
-    title_id VARCHAR(255)   NOT NULL,
-    title VARCHAR(255)   NOT NULL,
-    CONSTRAINT pk_Titles PRIMARY KEY (
-        title_id
+CREATE TABLE "titles" (
+    "title_id" VARCHAR(6)   NOT NULL,
+    "title" VARCHAR(255)   NOT NULL,
+    CONSTRAINT "pk_titles" PRIMARY KEY (
+        "title_id"
      )
 );
 
-ALTER TABLE Employees ADD CONSTRAINT fk_Employees_emp_title_id FOREIGN KEY(emp_title_id)
-REFERENCES Titles (title_id);
+ALTER TABLE "employees" ADD CONSTRAINT "fk_employees_emp_title_id" FOREIGN KEY("emp_title_id")
+REFERENCES "titles" ("title_id");
 
-ALTER TABLE Dept_managers ADD CONSTRAINT fk_Dept_managers_dept_no FOREIGN KEY(dept_no)
-REFERENCES Departments (dept_no);
+ALTER TABLE "dept_employees" ADD CONSTRAINT "fk_dept_employees_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employees" ("emp_no");
 
-ALTER TABLE Dept_managers ADD CONSTRAINT fk_Dept_managers_emp_no FOREIGN KEY(emp_no)
-REFERENCES Employees (emp_no);
+ALTER TABLE "dept_employees" ADD CONSTRAINT "fk_dept_employees_dept_no" FOREIGN KEY("dept_no")
+REFERENCES "departments" ("dept_no");
 
-ALTER TABLE Dept_employees ADD CONSTRAINT fk_Dept_employees_emp_no FOREIGN KEY(emp_no)
-REFERENCES Employees (emp_no);
+ALTER TABLE "dept_managers" ADD CONSTRAINT "fk_dept_managers_dept_no" FOREIGN KEY("dept_no")
+REFERENCES "departments" ("dept_no");
 
-ALTER TABLE Dept_employees ADD CONSTRAINT fk_Dept_employees_dept_no FOREIGN KEY(dept_no)
-REFERENCES Departments (dept_no);
+ALTER TABLE "dept_managers" ADD CONSTRAINT "fk_dept_managers_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employees" ("emp_no");
 
-ALTER TABLE Salaries ADD CONSTRAINT fk_Salaries_emp_no FOREIGN KEY(emp_no)
-REFERENCES Employees (emp_no);
+ALTER TABLE "salaries" ADD CONSTRAINT "fk_salaries_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employees" ("emp_no");
 
 ------ 2. Data Analysis 
 --- List the employee number, last name, first name, sex, and salary of each employee.
